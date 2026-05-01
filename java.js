d4e1160c211e5ddca1b9443b56384f29d2ffe596
chanelchandra <chanelchandra.aus@gmail.com>
2026-05-01 10:11:04 +0000

    lockscreen editing

--- java.js
+++ java.js
@@ -2,12 +2,21 @@
 function startTime() {
     const today = new Date();
 
-    // Get local time string (e.g., "12:30:45 PM")
+    // Get local time string
     let timeString = today.toLocaleTimeString('en-GB', {
         hour: '2-digit',
         minute: '2-digit',
         hour12: false
     });
+
+    let dateString = today.toLocaleDateString('en-GB', {
+        weekday: 'long', 
+        day: 'numeric', 
+        month: 'long', 
+        year: 'numeric'
+    })
+
+    document.getElementById('date').innerHTML = dateString;
             
     // Update the HTML element with the new time
     document.getElementById('clock').innerHTML = timeString;
